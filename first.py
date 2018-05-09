8080161a1ac533d85008b4276fcc96027693c2c0
nekonee <haniasa96@gmail.com>
2018-05-09 22:54:48 +0200

    Random tunnel and room generator, changed ground colour to lighter one

--- first.py
+++ first.py
@@ -2,11 +2,17 @@ import libtcodpy as lib
 
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
+
 MAP_WIDTH = 80
-MAP_HEIGHT = 45
+MAP_HEIGHT = 50
+
+ROOM_MAX_SIZE = 15
+ROOM_MIN_SIZE = 5
+MAX_ROOMS = 20
 
 color_dark_wall = lib.Color(0, 0, 100)
 color_dark_ground = lib.Color(50, 50, 150)
+color_light_ground = lib.Color(246, 192, 221)
 
 LIMIT_FPS = 20
 
@@ -17,6 +23,16 @@ class Rectangle:
         self.top_left_y = y
         self.bottom_right_x = x+w
         self.bottom_right_y = y+h
+        
+
+     def centering(self):
+         center_x = (self.top_left_x + self.bottom_right_x) / 2
+         center_y = (self.top_left_y + self.bottom_right_y) / 2
+         return(center_x, center_y)
+
+     def check_intersection(self, other):
+         return(self.top_left_x <= other.bottom_right_x and self.bottom_right_x >= other.top_left_x and self.top_left_y <= other.bottom_right_y and self.bottom_right_y >= other.top_left_y)
+
 
 class Tile:
     def __init__(self, blocked, block_sight=None):
@@ -44,15 +60,6 @@ class Character:
 
     def clear(self):
         lib.console_put_char(char_con, self.axis_X, self.axis_Y, ' ', lib.BKGND_NONE)
-
-def centering_tunnels(self):
-    center_x = (self.top_left_x + self.bottom_right_x) / 2
-    center_y = (self.top_left_y + self.bottom_right_y) / 2
-    return(center_x, center_y)
-        
-def check_intersection(self, other):
-    return(self.top_left_x <= other.bottom_right_x and self.bottom_right_x >= other.top_left_x and self.top_left_y <= other.bottom_right_y and self.bottom_right_y >= other.top_left_y)
-
         
 def horizontal_tunnel(x1, x2, y):
     global map
@@ -78,20 +85,48 @@ def draw_map():
     map = [[Tile(True)
             for hgt in range(MAP_HEIGHT)]
                 for wid in range(MAP_WIDTH) ]
-    first_room = Rectangle(20, 15, 10, 15)
-    second_room = Rectangle(50, 15, 10, 15)
-    create_room(first_room)
-    create_room(second_room)
-    horizontal_tunnel(25, 55, 20)
-    horizontal_tunnel(25, 55, 21)
-    horizontal_tunnel(25, 55, 22)
-    vertical_tunnel(10, 20, 27)
-    vertical_tunnel(10, 20, 28)
-    vertical_tunnel(10, 20, 29)
+    rooms = []
+    rooms_num = 0
+    for r in range(MAX_ROOMS):
+        wid = lib.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        hgt = lib.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        #random position without crossing map's boundaries
+        x = lib.random_get_int(0, 0, MAP_WIDTH - wid - 1)
+        y = lib.random_get_int(0, 0, MAP_HEIGHT - hgt - 1)
+
+        new_room = Rectangle(x, y, wid, hgt)
+
+        fail = False
+        for other_room in rooms:
+             if new_room.check_intersection(other_room):
+                  fail = True
+                  break
+        if not fail:
+             #no intersections, so room is valid
+             create_room(new_room)
+             (new_x, new_y) = new_room.centering()
+              #first room, putting player in it
+             if rooms_num == 0:
+                  player.axis_X = new_x
+                  player.axis_Y = new_y
+             else:
+                  #other rooms + connecting them with tunnels
+                  #centering coordinates of previous room
+                  (previous_x, previous_y) = rooms[rooms_num - 1].centering()
+                  if lib.random_get_int(0, 0, 1) == 1:
+                       horizontal_tunnel(previous_x, new_x, previous_y)
+                       vertical_tunnel(previous_y, new_y, previous_x)
+                  else:
+                       vertical_tunnel(previous_y, new_y, previous_x)
+                       horizontal_tunnel(previous_x, new_x, previous_y)
+
+        rooms.append(new_room)
+        rooms_num += 1
 
 def render_all():
     global color_dark_wall
     global color_dark_ground
+    global color_light_ground
 
     for h in range(MAP_HEIGHT):
         for w in range(MAP_WIDTH):
@@ -99,7 +134,7 @@ def render_all():
             if wall:
                 lib.console_put_char_ex(char_con, w, h, '#', color_dark_wall, lib.BKGND_SET)
             else:
-                lib.console_put_char_ex(char_con, w, h, '.', color_dark_ground, lib.BKGND_SET)
+                lib.console_put_char_ex(char_con, w, h, '.', color_light_ground, lib.BKGND_SET)
                     
     for object in objects:
         object.draw()
@@ -135,7 +170,9 @@ lib.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'LANDS OF JO-EBS', False)
 lib.sys_set_fps(LIMIT_FPS)
 char_con = lib.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-player = Character(25, 23, '@', lib.white)
+player = Character(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', lib.white)
+#SCREEN_WIDTH/2, SCREEN_HEIGHT/2
+#25, 23
 objects=[player]
 
 draw_map()
