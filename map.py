2039a49f58bc5b292458ea6a9dd78bc77b9f4c2e
nekonee <haniasa96@gmail.com>
2018-05-20 01:45:31 +0200

    Base files for creating the map and placing the characters

---
+++ map.py
@@ -0,0 +1,10 @@
+import libtcodpy as lib
+
+from const import const
+import classes
+
+
+#generating map - use from classes
+
+class Map:
+    def __init__(self)
---
+++ placement.py
@@ -0,0 +1,5 @@
+from loader import loader
+from classes import Character
+from map import Map
+
+#place the map and characters
