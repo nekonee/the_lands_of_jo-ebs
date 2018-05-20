38a9ac50b2f5a5995143f67076cffdc3699ee340
nekonee <haniasa96@gmail.com>
2018-05-20 20:49:49 +0200

    Drawing map with tunnels

--- map.py
+++ map.py
@@ -1,5 +1,6 @@
 import libtcodpy as lib
 
+from functions import *
 from const import const
 import classes
 
@@ -7,7 +8,7 @@ import classes
 #generating map - use from classes
 
 class Map:
-    def __init__(self, min_size, max_size):
+    def __init__(self, min_size, max_size, room):
         self.width = lib.random_get_int(0, const.ROOM_MIN_SIZE, const.ROOM_MAX_SIZE)
         self.height = lib.random_get_int(0, const.ROOM_MIN_SIZE, const.ROOM_MAX_SIZE)
         #random position without crossing map's borders
@@ -17,9 +18,18 @@ class Map:
     def get_tile(self, blocked, block_sight = None):
         tile = classes.Tile(blocked, block_sight = None)
 
-    def create_room(self):
+        
+    def create_room(self, room):
+        global map
+        #room = rectangle class
+        for x in range(room.top_left_x + 1, room.bottom_right_x):
+            for y in range(room.top_left_y + 1, room.bottom_right_y):
+                map[x][y].blocked = False
+                map[x][y].block_sight = False
+        
 
     def draw_map(self, pos_x, pos_y, width, height):
+        global map
         map = [[Tile(True)
                 for hgt in range(MAP_HEIGHT)]
                     for wid in range(MAP_WIDTH) ]
@@ -36,5 +46,20 @@ class Map:
                     break
                 #no intersections, valid room
             if not fail:
-                
-                
+                self.create_room(new_room)
+                (new_x, new_y) = new_room.centering()
+                if rooms_num == 0:
+                    player.axis_X = new_x
+                    player.axis_Y = new_y
+                else:
+                    #there are other rooms, you should connect them with tunnels
+                    (previous_x, previous_y) = rooms(rooms_num - 1).centering()
+                    if lib.random_get_int(0, 0, 1) == 1:
+                        horizontal_tunnel(previous_x, new_x, previous_y)
+                        vertical_tunnel(previous_y, new_y, previous_x)
+                    else:
+                        vertical_tunnel(previous_y, new_y, previous_x)
+                        horizontal_tunnel(previous_x, new_x, previous_y)
+
+            rooms.append(new_room)
+            rooms_num += 1
