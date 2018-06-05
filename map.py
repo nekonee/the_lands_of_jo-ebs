3caa5a03ea7923c76b683a825f06992cbf471ca8
nekonee <haniasa96@gmail.com>
2018-06-05 23:30:11 +0200

    Updated map file

--- map.py
+++ map.py
@@ -1,9 +1,9 @@
 import tcod
 
-from functions import *
 from const import const
 from globs import globs
 import classes
+import functions
 
 
 #generating map - use from classes
@@ -16,36 +16,37 @@ class Map:
     else:
         map = map
     
-    def __init__(self, min_size, max_size, room):
-        self.width = tcod.random_get_int(0, const.ROOM_MIN_SIZE, const.ROOM_MAX_SIZE)
-        self.height = tcod.random_get_int(0, const.ROOM_MIN_SIZE, const.ROOM_MAX_SIZE)
-        #random position without crossing map's borders
-        self.pos_x = tcod.random_get_int(0, 0, const.MAP_WIDTH - width - 1)
-        self.pos_y - tcod.random_get_int(0, 0, const.MAP_HEIGHT - height - 1)
+    def __init__(self):
+        self.width = tcod.random_get_int(0, const['ROOM_MIN_SIZE'], const['ROOM_MAX_SIZE'])
+        self.height = tcod.random_get_int(0, const['ROOM_MIN_SIZE'], const['ROOM_MAX_SIZE'])
 
+        self.pos_x = tcod.random_get_int(0, 0, (const['MAP_WIDTH'] - 1 - self.width))
+        self.pos_y = tcod.random_get_int(0, 0, (const['MAP_HEIGHT'] - 1 - self.height))
+        
     def get_tile(self, blocked, block_sight = None):
         tile = classes.Tile(blocked, block_sight = None)
 
         
-    def create_room(self, room):
+    def create_room(self):
         global map
         #room = rectangle class
-        for x in range(room.top_left_x + 1, room.bottom_right_x):
-            for y in range(room.top_left_y + 1, room.bottom_right_y):
+        for x in range(room.top_x + 1, room.bottom_x):
+            for y in range(room.top_y + 1, room.bottom_y):
                 map[x][y].blocked = False
                 map[x][y].block_sight = False
         
 
-    def draw_map(self, pos_x, pos_y, width, height):
+    def draw_map(self):
         global map
-        map = [[Tile(True)
+        map = [[classes.Tile(True)
                 for hgt in range(MAP_HEIGHT)]
                     for wid in range(MAP_WIDTH) ]
         rooms = []
         rooms_num = 0
 
-        for r in range(const.MAX_ROOMS):
-            new_room = classes.Rectangle(pos_x, pos_y, width, height)
+        for r in range(const['MAX_ROOMS']):
+            #those values should be random
+            new_room = classes.Rectangle(self.pos_x, self.pos_y, self.width, self.height)
 
             fail = False
             for other_room in rooms:
@@ -63,11 +64,11 @@ class Map:
                     #there are other rooms, you should connect them with tunnels
                     (previous_x, previous_y) = rooms(rooms_num - 1).centering()
                     if tcod.random_get_int(0, 0, 1) == 1:
-                        horizontal_tunnel(previous_x, new_x, previous_y)
-                        vertical_tunnel(previous_y, new_y, previous_x)
+                        functions.horizontal_tunnel(previous_x, new_x, previous_y)
+                        functions.vertical_tunnel(previous_y, new_y, previous_x)
                     else:
-                        vertical_tunnel(previous_y, new_y, previous_x)
-                        horizontal_tunnel(previous_x, new_x, previous_y)
+                        functions.vertical_tunnel(previous_y, new_y, previous_x)
+                        functions.horizontal_tunnel(previous_x, new_x, previous_y)
 
             rooms.append(new_room)
             rooms_num += 1
