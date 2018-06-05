87634c95fde5bed915c7586b7cc0bcb559640f4a
nekonee <haniasa96@gmail.com>
2018-06-05 23:31:28 +0200

    Basic placement

--- placement.py
+++ placement.py
@@ -1,6 +1,17 @@
+import tdl
 from loader import loader
-from classes import Character
+import classes
 from map import Map
 from globs import globs
+from const import const
 
 #place the map and characters
+
+
+def place_player():
+    player = classes.Character(globs['SCREEN_WIDTH']/2 , globs['SCREEN_HEIGHT'])
+
+
+def get_map():
+    curr_map = Map()
+    
