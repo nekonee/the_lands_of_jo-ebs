6a453872dee7ff55e58d0c7875e48ba4e4b70992
nekonee <haniasa96@gmail.com>
2018-06-08 00:01:30 +0200

    Initializing fov_map and player in globs

--- placement.py
+++ placement.py
@@ -3,8 +3,14 @@ import classes
 from const import const
 from globs import globs
 
-def initialize_glob_player():
-    globs['player'] = place_player()
+
+def initialize_fov_map():
+    const['FOV_MAP'] = tcod.map_new(const['MAP_WIDTH'], const['MAP_HEIGHT'])
+
 
 def place_player():
     player = classes.Character(const['SCREEN_WIDTH']/2 , const['SCREEN_HEIGHT']/2, '@', const['color_player'], blocks = True)
+
+
+def initialize_glob_player():
+    globs['player'] = place_player()
