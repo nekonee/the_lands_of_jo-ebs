915f4b0903dc7dc82aed50dd8ee18ca2658d3fe5
nekonee <haniasa96@gmail.com>
2018-06-11 17:35:45 +0200

    Changing lib to tcod and passing arguments to functions

--- functions.py
+++ functions.py
@@ -22,27 +22,28 @@ def vertical_tunnel(y1, y2, x):
 
 def render_all(fov, player, map, fov_map, char_con, objects):
     #call it with fov_recompute
-    if fov_map:
-        fov_map = False
-        tcod.map_compute_fov(fov_map, player.axis_X, player.axis_Y, const['TORCH_RADIUS'],const['FOV_LIGHT_VALLS'], const['FIELD_OF_VIEW_ALGO'])
+    if fov:
+        fov = False
+        light_walls = int(const['FOV_LIGHT_WALLS'])
+        tcod.map_compute_fov(fov_map, int(player.axis_X), int(player.axis_Y), const['TORCH_RADIUS'], light_walls, const['FIELD_OF_VIEW_ALGO'])
 
         for h in range(const['MAP_HEIGHT']):
             for w in range(const['MAP_WIDTH']):
-                visible = lib.map_is_in_fov(fov_map, w, h)
+                visible = tcod.map_is_in_fov(fov_map, w, h)
                 wall = map[w][h].block_sight
                 if not visible:
                    if  map[w][h].explored:
                     #not visible and not explored- everything is black
-                    lib.console_set_char_background(char_con, w, h, const['color_dark_wall'])
+                    tcod.console_set_char_background(char_con, w, h, const['color_dark_wall'])
                 else:
                     #is visible
                     if wall:
-                        lib.console_set_char_ex(char_con, w, h, '#', const['color_light_wall'], lib.BKGND_SET)
+                        tcod.console_set_char_ex(char_con, w, h, '#', const['color_light_wall'], tcod.BKGND_SET)
                     else:
                         #ground
-                        lib.console_put_char_ex(char_con, w, h, '.', const['color_ground'], lib.BKGND_SET)
+                        tcod.console_put_char_ex(char_con, w, h, '.', const['color_ground'], tcod.BKGND_SET)
 
             for object in objects:
-                object.draw()
+                object.draw(fov_map, char_con)
 
-            lib.console_blit(char_con, 0, 0, const['SCREEN_WIDTH'], const['SCREEN_HEIGHT'], 0, 0, 0)
+            tcod.console_blit(char_con, 0, 0, const['SCREEN_WIDTH'], const['SCREEN_HEIGHT'], 0, 0, 0)
